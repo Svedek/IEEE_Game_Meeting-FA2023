24d99a09e4f4040c254fc414c34ef616b55ce75a
Svedek <28320440+Svedek@users.noreply.github.com>
2023-10-19 19:25:54 -0500

    Player movement data exported

--- IEEE-CS_game_meeting/Scenes/Player.gd
+++ IEEE-CS_game_meeting/Scenes/Player.gd
@@ -1,11 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+@export var gravity:float = 980.0
 
 
 func _physics_process(delta):
